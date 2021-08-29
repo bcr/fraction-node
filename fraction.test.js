916b3fc07f2fa3f1de5a7a90ac52bdf8272c4c73
Blake Ramsdell <blaker@gmail.com>
2021-08-29 13:39:21 -0700

    Test cleanup

--- fraction.test.js
+++ fraction.test.js
@@ -3,22 +3,26 @@ const fraction = require('./fraction');
 test('does nothing', () => {
 })
 
-test('computes first example', () => {
-    expect(fraction.evaluate('1/2 * 3_3/4')).toBe('1_7/8')
+describe("evaluate", () => {
+    test('computes first example', () => {
+        expect(fraction.evaluate('1/2 * 3_3/4')).toBe('1_7/8')
+    })
+
+    test('computes second example', () => {
+        expect(fraction.evaluate('2_3/8 + 9/8')).toBe('3_1/2')
+    })
 })
 
-test('computes second example', () => {
-    expect(fraction.evaluate('2_3/8 + 9/8')).toBe('3_1/2')
-})
+describe("parse", () => {
+    test('parses fraction', () => {
+        expect(fraction.parse('1/2')).toStrictEqual({ numerator: 1, denominator: 2 })
+    })
 
-test('parses fraction', () => {
-    expect(fraction.parse('1/2')).toStrictEqual({ numerator: 1, denominator: 2 })
-})
+    test('parses mixed number', () => {
+        expect(fraction.parse('3_3/4')).toStrictEqual({ numerator: 15, denominator: 4 })
+    })
 
-test('parses mixed number', () => {
-    expect(fraction.parse('3_3/4')).toStrictEqual({ numerator: 15, denominator: 4 })
-})
-
-test('parses whole number', () => {
-    expect(fraction.parse('3')).toStrictEqual({ numerator: 3, denominator: 1 })
+    test('parses whole number', () => {
+        expect(fraction.parse('3')).toStrictEqual({ numerator: 3, denominator: 1 })
+    })
 })
