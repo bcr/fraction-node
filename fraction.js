e2f03b38deb88e7f1aebcc6856a16431dd9fbcc1
Blake Ramsdell <blaker@gmail.com>
2021-08-29 12:15:58 -0700

    First cut of parsing

--- fraction.js
+++ fraction.js
@@ -1,5 +1,35 @@
-function evaluate(expression) {
-    return '1_7/8'
+function parse(fractionString) {
+    const hasWhole = fractionString.includes('_')
+    const hasFraction = fractionString.includes('/')
+    let whole = 0
+    let numerator = 0
+    let denominator = 1
+    let fractionPart = fractionString
+
+    if (hasWhole) {
+        const parts = fractionString.split('_')
+        whole = parseInt(parts[0])
+        fractionPart = parts[1]
+    }
+
+    if (hasFraction) {
+        const parts = fractionPart.split('/')
+        numerator = parseInt(parts[0])
+        denominator = parseInt(parts[1])
+    }
+
+    return { numerator: whole * denominator + numerator, denominator: denominator }
 }
 
+function evaluate(expression) {
+    let parts = expression.split(/\s+/)
+    const fraction1 = parse(parts[0])
+    const operation = parts[1]
+    const fraction2 = parse(parts[2])
+
+    return fraction2
+}
+
+// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 exports.evaluate = evaluate
+exports.parse = parse
--- fraction.test.js
+++ fraction.test.js
@@ -3,6 +3,18 @@ const fraction = require('./fraction');
 test('does nothing', () => {
 })
 
-test('computes first example', () => {
+test.skip('computes first example', () => {
     expect(fraction.evaluate('1/2 * 3_3/4')).toBe('1_7/8')
+})
+
+test.skip('computes second example', () => {
+    expect(fraction.evaluate('2_3/8 + 9/8')).toBe('3_1/2')
+})
+
+test('parses fraction', () => {
+    expect(fraction.parse('1/2')).toStrictEqual({ numerator: 1, denominator: 2 })
+})
+
+test('parses mixed number', () => {
+    expect(fraction.parse('3_3/4')).toStrictEqual({ numerator: 15, denominator: 4 })
 })
